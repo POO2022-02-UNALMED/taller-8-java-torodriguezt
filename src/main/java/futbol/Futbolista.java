5e2f425d2f3cfc5ee1e5c2ceadbc55baea1fa773
Tomás Rodríguez <torodriguezt@unal.edu.co>
2022-10-13 22:27:47 -0500

    Taller

--- src/main/java/futbol/Futbolista.java
+++ src/main/java/futbol/Futbolista.java
@@ -5,14 +5,14 @@ public abstract class Futbolista implements Comparable <Futbolista> {
 	private int edad;
 	private final String posicion;
 	
-	Futbolista(String nombre, String posicion, int edad){
+	Futbolista(String nombre,  int edad, String posicion){
 		this.nombre = nombre;
 		this.posicion = posicion;
 		this.edad = edad;
 
 	}
 	Futbolista(){
-		this("Maradona", "delantero", 30);
+		this("Maradona", 30, "delantero");
 	}
 	
 	public boolean equals(Futbolista f) {
