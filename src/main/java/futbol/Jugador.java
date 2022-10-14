d9ac6a0616e7d7afc39270da11b5645ea3cc5b79
Tomás Rodríguez <torodriguezt@unal.edu.co>
2022-10-14 15:43:23 -0500

    Taller

--- src/main/java/futbol/Jugador.java
+++ src/main/java/futbol/Jugador.java
@@ -3,7 +3,7 @@ public class Jugador extends Futbolista {
 	public short golesMarcados;
 	public byte dorsal;
 	
-	public Jugador(String nombre, String posicion, int edad, short golesMarcados, byte dorsal){
+	public Jugador(String nombre, int edad, String posicion, short golesMarcados, byte dorsal){
 		super(nombre, edad, posicion);
 		this.golesMarcados = golesMarcados;
 		this.dorsal = dorsal;
